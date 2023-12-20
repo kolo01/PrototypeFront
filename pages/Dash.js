6481c44de46e219fffc50019d2dc1cc9bed6d272
kolo01 <konedieu5@gmail.com>
2023-12-20 21:05:37 +0100

    okay

--- pages/Dash.js
+++ pages/Dash.js
@@ -1,12 +1,11 @@
 import Dashboard from '../components/dashboard/Dashboard';
 import Layout from '../components/layout/Layout';
-import SignInSide from '../components/loginRegister/Login';
 
 
 export default function Index() {
   return (
-    
-      <SignInSide/>
-  
+    <Layout title='Administrateur'>
+        <Dashboard />
+    </Layout>
   )
 }
--- pages/Register.js
+++ pages/Register.js
@@ -1,12 +1,13 @@
 import Dashboard from '../components/dashboard/Dashboard';
 import Layout from '../components/layout/Layout';
 import SignInSide from '../components/loginRegister/Login';
+import SignUp from '../components/loginRegister/Register';
 
 
 export default function Index() {
   return (
     
-      <SignInSide/>
+      <SignUp/>
   
   )
 }
